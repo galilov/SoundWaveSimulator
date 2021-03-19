29f7d7eed0b2c6584e6c654ac81bf31f189ad62d
Alexander Galilov <alexander.galilov@gmail.com>
2021-03-19 14:32:20 +0400

    Initial commit

---
+++ SoundWaveSimulator/MainForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace SoundWaveSimulator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Picture = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.WavePicture = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WavePicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Picture
+            // 
+            this.Picture.BackColor = System.Drawing.Color.White;
+            this.Picture.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Picture.Location = new System.Drawing.Point(0, 0);
+            this.Picture.Name = "Picture";
+            this.Picture.Size = new System.Drawing.Size(1683, 476);
+            this.Picture.TabIndex = 0;
+            this.Picture.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackBar
+            // 
+            this.trackBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.trackBar.Location = new System.Drawing.Point(0, 476);
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(1683, 69);
+            this.trackBar.TabIndex = 1;
+            this.trackBar.ValueChanged += new System.EventHandler(this.trackBar_ValueChanged);
+            // 
+            // WavePicture
+            // 
+            this.WavePicture.BackColor = System.Drawing.Color.Black;
+            this.WavePicture.Dock = System.Windows.Forms.DockStyle.Top;
+            this.WavePicture.Location = new System.Drawing.Point(0, 545);
+            this.WavePicture.Name = "WavePicture";
+            this.WavePicture.Size = new System.Drawing.Size(1683, 528);
+            this.WavePicture.TabIndex = 2;
+            this.WavePicture.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 1097);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 58);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(246, 1102);
+            this.trackBar1.Maximum = 30;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(550, 69);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 10;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1683, 1183);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.WavePicture);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.Picture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WavePicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Picture;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.PictureBox WavePicture;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ SoundWaveSimulator/MainForm.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SoundWaveSimulator
+{
+    public partial class MainForm : Form
+    {
+        private const double InitialOmegaT = 0.01 * Math.PI;
+
+        private int _waveOffset = 0;
+        private Bitmap _mic, _speaker;
+        private int _micPosition = 0;
+        private double _omegaT = InitialOmegaT;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _mic = new Bitmap("se1mic-1.png");
+            _speaker = new Bitmap("emmiter1-1.png");
+            trackBar.Minimum = _speaker.Width;
+            trackBar.Maximum = Picture.Width - _mic.Width;
+            trackBar.Value = trackBar.Maximum;
+            //Picture.Height = Math.Max(_mic.Height, _speaker.Height);
+            Picture.Paint += Picture_Paint;
+            WavePicture.Paint += WavePicture_Paint;
+            Resize += Form1_Resize;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Picture.Invalidate();
+            WavePicture.Invalidate();
+            trackBar.Maximum = Picture.Width - _mic.Width;
+            trackBar.Value = Picture.Width - _mic.Width + _micPosition;
+        }
+
+        private void WavePicture_Paint(object sender, PaintEventArgs e)
+        {
+            var waveAmplitudeCoefficient = WavePicture.Height * 0.4;
+            var g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            g.FillRectangle(Brushes.Black, 0, 0, Picture.Width, Picture.Height);
+            var pointsSpk = new PointF[WavePicture.Width];
+            var pointsMic = new PointF[WavePicture.Width];
+            for (var x = 0; x < WavePicture.Width; x++)
+            {
+                pointsSpk[x] = new PointF(x, (float)(WavePicture.Height / 2 + waveAmplitudeCoefficient * Sine(x + GetXSpkMembranaAvg())));
+                pointsMic[x] = new PointF(x, (float)(WavePicture.Height / 2 + waveAmplitudeCoefficient * Sine(x + GetXMicMembranaAvg())));
+            }
+            g.DrawLines(Pens.Yellow, pointsSpk);
+            g.DrawLines(Pens.Aqua, pointsMic);
+        }
+
+        private void Picture_Paint(object sender, PaintEventArgs e)
+        {
+            var r = 3;
+            var n = 4;
+            var g = e.Graphics;
+            var particleBrush = Brushes.Silver;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            g.FillRectangle(Brushes.Black, 0, 0, Picture.Width, Picture.Height);
+            var rnd = new Random(_waveOffset);
+            for (var x = 0; x < (int)(_speaker.Width * 0.8); x++)
+            {
+                for (var i = 0; i < n / 2; i++)
+                {
+                    g.FillRectangle(particleBrush, x, rnd.Next(Picture.Height), r, r);
+                }
+            }
+            for (var x = (int)(_speaker.Width * 0.8); x < Picture.Width; x++)
+            {
+                var k = (int)(Sine(x) * n) + n;
+                for (var i = 0; i < k; i++)
+                {
+                    g.FillRectangle(particleBrush, x, rnd.Next(Picture.Height), r, r);
+                }
+            }
+            g.DrawImageUnscaled(_speaker, 0, (Picture.Height - _mic.Height) / 2);
+            g.DrawImageUnscaled(_mic, Picture.Width - _mic.Width + _micPosition, (Picture.Height - _mic.Height) / 2);
+
+
+            var membranaAmplitude = 20;
+            var membrataTop = Picture.Height * 0.1f;
+            var membranaBottom = Picture.Height * 0.8f;
+            var membranaWidth = 6;
+
+            var xSpkMembranaAvg = GetXSpkMembranaAvg();
+            var xSpkMembrana = (float)(xSpkMembranaAvg + Sine(xSpkMembranaAvg) * membranaAmplitude);
+            g.FillRectangle(Brushes.Yellow, xSpkMembrana - membranaWidth / 2, membrataTop, membranaWidth, membranaBottom);
+
+            var xMicMembranaAvg = GetXMicMembranaAvg();
+            var xMicMembrana = (float)(xMicMembranaAvg + Sine(xMicMembranaAvg) * membranaAmplitude);
+            g.FillRectangle(Brushes.Aqua, xMicMembrana - membranaWidth / 2, membrataTop, membranaWidth, membranaBottom);
+        }
+
+        private int GetXMicMembranaAvg()
+        {
+            return Picture.Width - _mic.Width / 2 + _micPosition;
+        }
+
+        private int GetXSpkMembranaAvg()
+        {
+            return _speaker.Width - 15;
+        }
+
+        private double Sine(int x)
+        {
+            return Math.Sin((x + _waveOffset) * _omegaT);
+        }
+
+        private void trackBar_ValueChanged(object sender, EventArgs e)
+        {
+            _micPosition = trackBar.Value - (Picture.Width - _mic.Width);
+            if (!timer1.Enabled)
+            {
+                Picture.Invalidate();
+                WavePicture.Invalidate();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            _omegaT = trackBar1.Value * InitialOmegaT / 10;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            _waveOffset -= 5;
+            Picture.Invalidate();
+            WavePicture.Invalidate();
+        }
+    }
+}
